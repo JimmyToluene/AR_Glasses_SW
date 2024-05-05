9fff9400e1272de79bb3366e9feba2fd4bc686d7
JimmyToluene <52461070+JimmyToluene@users.noreply.github.com>
2024-05-05 17:40:13 +0800

    Version 4.5

--- navi_interface.py
+++ navi_interface.py
@@ -33,7 +33,7 @@ class NaviFrame:
         inner_circle = self.canvas.create_oval(55, 55, 145, 145, fill='white', outline='')
 
         self.speed_limit_number = tk.Label(self.navi_frame, text="50", bg="white", font=("Helvetica", 40, 'bold'))
-        self.speed_limit_number.place(x=240, y=260, anchor="center")
+        self.speed_limit_number.place(x=240, y=255, anchor="center")
         self.direction_label = tk.Label(self.navi_frame, text="", fg="white", bg="black", font=("Helvetica", 27))
         self.direction_label.place(x=470, y=240, anchor="center")
         self.eta_road = tk.Label(self.navi_frame, text="", fg="white", bg="black", font=("Helvetica", 33))
