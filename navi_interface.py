1544e9af355733ac5992bf5b5c512a63f9de6765
JimmyToluene <52461070+JimmyToluene@users.noreply.github.com>
2024-05-05 17:26:11 +0800

    Version 4.5

--- navi_interface.py
+++ navi_interface.py
@@ -2,8 +2,7 @@ import main_frame
 import tkinter as tk
 from ttkbootstrap import ttk
 from PIL import ImageTk, Image
-from NavigationSub import moread
-import threading
+
 
 class NaviFrame:
     def __init__(self, root, q):
@@ -25,18 +24,17 @@ class NaviFrame:
         self.navi_frame = tk.Frame(self.root, width=640, height=400)
         self.navi_frame.configure(background="black")
         self.navi_frame.pack(anchor='center', fill="both", expand=True)
-        self.canvas = tk.Canvas(self.navi_frame, width=640,height=400, bg='black')
-        self.canvas.pack(fill="both",expand=True)
-        self.canvas.create_oval(80, 80, 280, 280, outline="red", width=10)
+
 
     def create_information_display_widget(self):
-        self.speed_limit_number = tk.Label(self.navi_frame, text="", fg="white", bg="black",font=("Helvetica", 70))
-        self.speed_limit_number.place(x=180, y=170, anchor="center")
-        self.speed_limit_tag_label = tk.Label(self.navi_frame, text="Km/H", fg="white", bg="black", font=("Helvetica", 35))
-        self.speed_limit_tag_label.place(x=180, y=230, anchor="center")
-        self.direction_label = tk.Label(self.navi_frame, text="", fg="white", bg="black",font=("Helvetica", 27))
+        self.canvas = tk.Canvas(self.navi_frame, width=200, height=200, bg='black',bd=0, highlightthickness=0,relief='ridge')
+        outer_circle = self.canvas.create_oval(50, 50, 150, 150, outline='red', width=10)
+        inner_circle = self.canvas.create_oval(55, 55, 145, 145, fill='white', outline='')
+        self.speed_limit_number = tk.Label(self.canvas, text="50", bg="white", font=("Helvetica", 50, 'bold'))
+        self.speed_limit_number.place(x=100, y=100, anchor="center")
+        self.direction_label = tk.Label(self.navi_frame, text="", fg="white", bg="black", font=("Helvetica", 27))
         self.direction_label.place(x=470, y=240, anchor="center")
-        self.eta_road = tk.Label(self.navi_frame, text="", fg="white", bg="black",font=("Helvetica", 33))
+        self.eta_road = tk.Label(self.navi_frame, text="", fg="white", bg="black", font=("Helvetica", 33))
         self.eta_road.place(x=470, y=290, anchor="center")
         self.direction_icon = tk.Label(self.navi_frame, image="", bg="black")
         self.direction_icon.place(x=470, y=100, anchor="center")
@@ -46,11 +44,12 @@ class NaviFrame:
         try:
             while not self.queue.empty():
                 data = self.queue.get_nowait()
-                message = f"Speed Limit: {data['speed_limit']} km/h, Action: {data['action']}, Distance: {data['distance']}"
+                self.canvas.place(x=240, y=250, anchor='center')
                 self.speed_limit_number.config(text=data['speed_limit'])
+                message = f"Speed Limit: {data['speed_limit']} km/h, Action: {data['action']}, Distance: {data['distance']}"
                 self.direction_label.config(text=data['action'])
-                self.eta_road.config(text=data['distance'])
-            try :
+                self.eta_road.config(text=data['distance'] + " left")
+            try:
                 file_name = f"./icon/navi_icon/png/{data['direction_code']}.png"
                 img = Image.open(file_name)
                 photo = ImageTk.PhotoImage(img)
@@ -61,4 +60,3 @@ class NaviFrame:
             self.navi_frame.after(500, self.update_information)
         except Exception as e:
             print(f"An error occurred: {e}")
-
